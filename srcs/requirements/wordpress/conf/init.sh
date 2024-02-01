f882d94750d6fc2b2dd5a9f9724ba8b2c7d46b43
nmilan <nmilan@student.42lyon.fr>
2024-02-01 19:23:54 +0100

    add wordpress conf and scritp and fix cgi problem dl php

---
+++ srcs/requirements/wordpress/conf/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /var/www/wordpress
+
+if [ ! -f wp-config.php ]; then
+
+wp core download --allow-root
+wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PW --dbhost=$DB_HOST
+wp core install --allow-root --url=$DN --title=$WP_TITLE --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_MAIL
+
+wp user create --allow-root $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PW
+
+chown www-data:www-data /var/www/wordpress/wp-content/uploads -R
+
+fi
+
+php-fpm7.4 -F
